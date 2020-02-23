90d489b2c1337d6e75a355819c3ca0d595dacf8c
Luís Soares <lsoares@gmail.com>
2020-02-23 19:51:30 +0000

    List LED lamps

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,13 +1,28 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Controls;
 
 namespace my_lights
 {
     public partial class MainWindow : Window
     {
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show("Clicked");
+        private readonly Yeelights _yee = new Yeelights();
+
+        public MainWindow() {
+            _yee.discover((o, args) => {
+                Dispatcher?.Invoke(() => {
+                    Button button = new Button {Content = args.Device.Hostname.ToString()};
+                    splMain.Children.Add(button);
+                    button.Click += (sender1, eventArgs) => {
+                        Dispatcher?.Invoke(async () => {
+                            await args.Device.TurnOff();
+                            args.Device.SupportedOperations.ForEach((el) => Console.WriteLine(el));
+                        });
+                    };
+                });
+            });
         }
+
+        private void btnAddMore_Click(object sender, RoutedEventArgs e) { }
     }
 }
--- Yeelights.cs
+++ Yeelights.cs
@@ -1,7 +1,12 @@
-﻿namespace my_lights
+﻿using YeelightAPI;
+
+namespace my_lights
 {
     public class Yeelights
     {
-        
+        public void discover(DeviceLocator.DeviceFoundEventHandler deviceFound) {
+            DeviceLocator.OnDeviceFound += deviceFound;
+            DeviceLocator.Discover();
+        }
     }
 }
