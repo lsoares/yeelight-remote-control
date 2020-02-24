940587d686100df432c33b1c73e33110afd6f557
Luís Soares <lsoares@gmail.com>
2020-02-24 19:46:17 +0000

    Set brightness level

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Windows;
 using System.Windows.Controls;
+using Newtonsoft.Json;
 
 namespace my_lights
 {
@@ -10,15 +11,30 @@ namespace my_lights
 
         public MainWindow() {
             _yee.discover((o, args) => {
-                Dispatcher?.Invoke(() => {
+                Dispatcher?.Invoke(async () => {
+                    var row = new StackPanel();
+                    splMain.Children.Add(row);
+                    // on/off
                     var device = args.Device;
-                    Button button = new Button {Content = device.Hostname};
-                    splMain.Children.Add(button);
-                    button.Click += async (sender1, eventArgs) => {
-                        await device.Connect();
-                        await device.Toggle();
-                        device.Disconnect();
+                    if (!device.IsConnected) await device.Connect();
+
+                    Console.WriteLine(JsonConvert.SerializeObject(device));
+                    Button toggleBt = new Button {Content = device.Hostname + "(" + device.Name + ")"};
+                    toggleBt.Click += async (sender1, eventArgs) => { await device.Toggle(); };
+                    row.Children.Add(toggleBt);
+                    // increase/decrease brightness
+                    Slider brightnessSlider = new Slider {
+                        Minimum = 0, Maximum = 100, IsMoveToPointEnabled = true, IsSnapToTickEnabled = true,
+                        TickFrequency = 10
                     };
+                    brightnessSlider.ValueChanged += async (sender, eventArgs) => {
+                        var value = Convert.ToInt32(Math.Round(eventArgs.NewValue));
+                        await device.SetBrightness(value);
+                    };
+                    row.Children.Add(brightnessSlider);
+                    // set mode
+
+                    // set temp
                 });
             });
         }
