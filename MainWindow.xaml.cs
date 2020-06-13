7eb1e6be98a0f9eef9022922fccfb3b035b8596a
Luís Soares <lsoares@gmail.com>
2020-06-13 18:48:50 +0100

    Improve layout

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Windows;
+using System.Windows.Input;
 using YeelightAPI;
 
 namespace my_lights
 {
     public partial class MainWindow
     {
+        // TODO turn on/off all
         private void Window_Loaded(object sender, RoutedEventArgs e) {
             Dispatcher?.Invoke(async () =>
                 (await DeviceLocator.Discover()).ForEach(
@@ -16,6 +18,11 @@ namespace my_lights
 
         private void Exit(object sender, RoutedEventArgs e) {
             Environment.Exit(0);
+        }
+        
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.ChangedButton == MouseButton.Left) DragMove();
         }
     }
 }
