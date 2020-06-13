cb33f45e70d7348f64d3ca2d1499581725a507b4
Luís Soares <lsoares@gmail.com>
2020-06-13 22:21:02 +0100

    Log unhandled exceptions

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.IO;
 using System.Windows;
 using System.Windows.Input;
+using System.Windows.Threading;
 using YeelightAPI;
 
 namespace my_lights
@@ -8,7 +10,12 @@ namespace my_lights
     public partial class MainWindow
     {
         // TODO turn on/off all
+        // TODO refresh on show window
+        // TODO set .exe icon
+        // TODO disable controls for a second
+        // TODO double click also works
         private void Window_Loaded(object sender, RoutedEventArgs e) {
+            AppDomain.CurrentDomain.UnhandledException += OnUnhandledException;
             Dispatcher?.Invoke(async () =>
                 (await DeviceLocator.Discover()).ForEach(
                     device => MainContent.Children.Add(new CeilingLedControl(new CeilingLed(device)))
@@ -16,12 +23,16 @@ namespace my_lights
             );
         }
 
+        private void OnUnhandledException(object sender, UnhandledExceptionEventArgs e) {
+            using StreamWriter sw = File.AppendText("error.log");
+            sw.WriteLine(e.ExceptionObject);
+        }
+
         private void Exit(object sender, RoutedEventArgs e) {
             Environment.Exit(0);
         }
-        
-        private void Window_MouseDown(object sender, MouseButtonEventArgs e)
-        {
+
+        private void Window_MouseDown(object sender, MouseButtonEventArgs e) {
             if (e.ChangedButton == MouseButton.Left) DragMove();
         }
     }
